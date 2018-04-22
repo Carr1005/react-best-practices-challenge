256133bbc745265239d0d219e855404e376183ec
carr <kaiboy6302@gmail.com>
2018-04-22 11:18:16 +0800

    Mains.js & Button.js & Card.js

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 class Button extends Component {
   render() {
     return (
-     <a href="#" className={"cusbtn btn my-2 "+this.props.buttonMeta.styleClass}>{this.props.buttonMeta.label}</a>
+     <a href="#" className={`cusbtn btn my-2 ${this.props.buttonMeta.styleClass}`}>{this.props.buttonMeta.label}</a>
     );
   }
 }
